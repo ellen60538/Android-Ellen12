1915a2280cece512aa5d4e2784581e68adbac882
user <ellen60538@gmail.com>
2020-03-15 16:25:02 +0800

    2020-03-15-16:00

--- app/src/main/java/tw/org/iii/ellen/ellen12/MainActivity.java
+++ app/src/main/java/tw/org/iii/ellen/ellen12/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         new Thread(){
             @Override
             public void run() {
-                receiveUDP() ;
+                receiveUDP() ; 
             }
         }.start();
     }
